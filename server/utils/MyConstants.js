3a3f7950cff19c7b59fa6f92dd9978c848dd7e5b
NghiaPhann <phanvotrongnghia1211@gmail.com>
2023-07-25 00:40:57 +0700

    vercel deploy

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+  DB_SERVER: 'shopping-online.wglxpmf.mongodb.net',
+  DB_USER: '207ct10204',
+  DB_PASS: '59v314844',
+  DB_DATABASE: 'shoppingonline',
+  EMAIL_USER: '<email_user>', // Microsoft mail service
+  EMAIL_PASS: '<email_pass>',
+  JWT_SECRET: '121120',
+  JWT_EXPIRES: '1800000', // in milliseconds
+};
+module.exports = MyConstants;
+
